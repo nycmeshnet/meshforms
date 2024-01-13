0d36bcec06edc43205c51f78b3c7703e04077c70
Will Nilges <wdn5796@rit.edu>
2024-01-13 13:04:36 -0700

    Got the dropdown, probably

--- app/add-form.tsx
+++ app/add-form.tsx
@@ -4,9 +4,19 @@ import { useFormState } from "react-dom";
 import { useFormStatus } from "react-dom";
 import { createTodo } from "@/app/actions";
 
+import Select from 'react-select'
+
+
 const initialState = {
   message: "",
 };
+
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
 
 interface Fields {
   first_name: string
@@ -25,6 +35,8 @@ export function AddForm() {
       <h2>Join NYC Mesh</h2>
       <input type="text" name="first_name" placeholder="First Name" required />
       <input type="text" name="last_name" placeholder="Last Name" required />
+
+      <Select name="flavour" options={options} />
       
       <button type="submit">Submit</button>
     </form>
