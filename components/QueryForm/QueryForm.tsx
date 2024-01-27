f22b2e8882a6978cdf915f5fb1933967103207f2
Willard Nilges <willardnilges@gmail.com>
2024-01-27 15:57:40 -0500

    AG Grid is cringe

---
+++ components/QueryForm/QueryForm.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import { useFormState } from "react-dom";
+import { useFormStatus } from "react-dom";
+import { submitQueryMemberForm } from "@/app/api";
+import { useRouter } from 'next/navigation'
+import { ErrorBoundary } from "react-error-boundary";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import Select from 'react-select'
+import { useState } from "react";
+const options = [
+  { value: "street_address", label: "Address" },
+  { value: "email_address", label: "Email" },
+  { value: "network_number", label: "Network Number" },
+  { value: "install_number", label: "Install Number" },
+  { value: "bin", label: "BIN" }
+]
+
+import { AgGridReact } from 'ag-grid-react'; // React Grid Logic
+import "ag-grid-community/styles/ag-grid.css"; // Core CSS
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Theme
+
+import styles from './QueryForm.module.scss'
+
+import { useState } from "react";
+
+
+export function QueryForm() {
+  async function sendForm(event: FormData) {
+    let query_type = event.get('query_type');
+    let email_address = event.get('email_address');
+    console.log(query_type);
+    console.log(email_address);
+    try {
+      setQueryComplete(true);
+      if (query_type === 'email_address') {
+        let resp = await submitQueryMemberForm(email_address);
+        toast.success('Success!', {
+          hideProgressBar: true,
+        });
+        console.log(resp);
+        setQueryResult(resp);
+      }
+    } catch (e) {
+      console.log("Could not submit Query Form: ");
+      console.log(e);
+      toast.error('Sorry, an error occurred.', {
+        hideProgressBar: true,
+        theme: "colored",
+      });
+      setQueryComplete(false);
+      return;
+    }
+  }
+
+  const initialState = {};
+  // const [state, formAction] = useFormState(createTodo, initialState);
+  const router = useRouter()
+  const [queryComplete, setQueryComplete] = useState(false);
+  const [queryType, setQueryType] = useState('select_query_type');
+  const [queryLabel, setQueryLabel] = useState('Select Query Type');
+
+  const [queryResult, setQueryResult] = useState(
+[
+    {
+      "install_number": -1,
+      "street_address": "",
+      "city": "",
+      "state": "",
+      "zip_code": "",
+      "unit": "",
+      "name": "",
+      "email_address": "",
+      "notes": "",
+      "network_number": -1,
+      "install_status": -1
+    }
+  ]
+    );
+
+  // Column Definitions: Defines & controls grid columns.
+  const [colDefs, setColDefs] = useState([
+    { field: "install_number" },
+    { field: "street_address" },
+    { field: "city" },
+    { field: "state" },
+    { field: "zip_code" },
+    { field: "unit" },
+    { field: "name" },
+    { field: "email_address" },
+    { field: "notes" },
+    { field: "network_number" },
+    { field: "install_status" },
+  ]);
+
+  return <>
+    <div className={styles.formBody}>
+      <form action={sendForm}>
+        <h2>MeshDB Query</h2>
+        <p>This is for installers to query our database. This is password protected.</p>
+        <br/>
+          <Select 
+            name="query_type"
+            placeholder="Query Type"
+            options={options}
+            className={styles.drop}
+            onChange={(selected) => {
+              setQueryType(selected.value);
+              setQueryLabel(selected.label);
+            }}
+          />
+          <div className={styles.horizontal}>
+            <input type="text" name={queryType} placeholder={queryLabel} required />
+            {/* TODO: <input type="password" name="password" placeholder="Password" required />*/}
+          </div>
+        <button className={styles.submitButton} type="submit">Submit</button>
+      </form>
+    </div>
+    <div className={styles.queryResultTable}>
+      <div className={"ag-theme-quartz"} style={{height: '400px', width: '600px'}}>
+        <AgGridReact rowData={queryResult} columnDefs={colDefs} />
+      </div>
+    </div>
+    <ToastContainer />
+  </>
+}
