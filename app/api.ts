5b7090fb339fa084ca6700abe6c055e27f0f96d5
Willard Nilges <willardnilges@gmail.com>
2024-01-27 15:57:40 -0500

    AG Grid is cringe

--- app/api.ts
+++ app/api.ts
@@ -38,6 +38,33 @@ export const NNAssignFormResponse = z.object({
 })
 export type NNAssignFormResponse = z.infer<typeof NNAssignFormResponse>
 
+export const QueryFormResponse = z.array(z.object({
+    install_number: z.number(),
+    street_address: z.string(),
+    city: z.string(),
+    state: z.string(),
+    zip_code: z.string(),
+    unit: z.string(),
+    name: z.string(),
+    email_address: z.string(),
+    notes: z.string(),
+    network_number: z.number().nullable(),
+    install_status: z.number(),
+}))
+export type QueryFormResponse = z.infer<typeof QueryFormResponse>
+
+const get = async <S extends z.Schema>(url: string, schema: S, auth?: string, nextOptions?: NextFetchRequestConfig): Promise<ReturnType<S['parse']>> => {
+  const res = await fetch(new URL(url, API_BASE), {
+    headers: {
+      ...auth && { Authorization: `Bearer ${auth}` },
+    },
+    next: nextOptions,
+  })
+    .catch(console.warn)
+  if (!res?.ok) throw res
+  return schema.parse(await res.json())
+}
+
 const post = async <S extends z.Schema>(url: string, schema: S, input: unknown, auth?: string, method = 'POST'): Promise<ReturnType<S['parse']>> => {
   console.log("Will POST: " + input)
   const res = await fetch(new URL(url, API_BASE), {
@@ -58,3 +85,5 @@ const API_BASE = new URL(process.env.NEXT_PUBLIC_MESHDB_URL as string)
 // TODO: Env var for api token
 export const submitJoinForm = (input: JoinFormInput) => post(`/api/v1/join/`, JoinFormResponse, JoinFormInput.parse(input))
 export const submitNNAssignForm = (input: NNAssignFormInput) => post(`/api/v1/nn-assign/`, NNAssignFormResponse, NNAssignFormInput.parse(input))
+
+export const submitQueryMemberForm = (input: str) => get(`/api/v1/query/member/?email_address=${input}`, QueryFormResponse)
---
+++ app/query/page.tsx
@@ -0,0 +1,15 @@
+import { Footer } from "@/components/Footer/Footer";
+import { Header } from "@/components/Header/Header";
+import { QueryForm } from "@/components/QueryForm/QueryForm";
+
+// TODO:
+// https://www.npmjs.com/package/react-phone-number-input
+// https://www.npmjs.com/package/react-error-boundary
+
+export default async function Join() {
+  return <>
+    <main>
+      <QueryForm />
+    </main>
+  </>
+}
