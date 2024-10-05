9b2b576cb58e539b78961fb7724f479f6aa6eb46
Willard Nilges <willardnilges@gmail.com>
2024-10-05 02:48:08 -0400

    Well I got a 200 but it's not working

--- components/PanoramaUpload/PanoramaUpload.tsx
+++ components/PanoramaUpload/PanoramaUpload.tsx
@@ -3,6 +3,10 @@
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 
+import { toastErrorMessage } from "@/app/utils/toastErrorMessage";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 type FormValues = {
   files: FileList;
 };
@@ -13,15 +17,44 @@ const PanoramaUploadForm: React.FC = () => {
   // Define the submit handler
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     const files = data.files;
+
+    
+    const formData = new FormData();
+    
+    Array.from(files).forEach(file => {
+      formData.append('files[]', file);
+    });
     
     // Process the files
     Array.from(files).forEach(file => {
       console.log(file.name);
       // TODO: Uplaod photos to S3 via Pano
+      fetch('http://127.0.0.1:8089', {
+        method: 'POST',
+        body: formData,
+      }).then((response) => {
+        if (response.ok) {
+          console.log('Files uploaded successfully');
+
+          toast.success("Upload Successful!", {
+            hideProgressBar: true,
+            theme: "colored",
+          });
+        }
+      }).catch((error) => {
+        console.error('File upload error:', error);
+
+        toast.error(`File upload error: ${error}`, {
+          hideProgressBar: true,
+          theme: "colored",
+        });
+      });
+
     });
   };
 
   return (
+    <>
     <form onSubmit={handleSubmit(onSubmit)}>
       <input
         type="file"
@@ -30,6 +63,10 @@ const PanoramaUploadForm: React.FC = () => {
       />
       <button type="submit">Upload</button>
     </form>
+    <div className="toasty">
+      <ToastContainer />
+    </div>
+    </>
   );
 };
 
