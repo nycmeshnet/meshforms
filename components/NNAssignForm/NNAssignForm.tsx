439be4d0a76df80c5ddcf0faa828bf293986d7af
Willard Nilges <willardnilges@gmail.com>
2024-10-24 08:01:48 -0400

    rewrite nn assign form

--- components/NNAssignForm/NNAssignForm.tsx
+++ components/NNAssignForm/NNAssignForm.tsx
@@ -1,17 +1,57 @@
 "use client";
 
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
+import { useForm, SubmitHandler } from "react-hook-form";
 import styles from "./NNAssignForm.module.scss";
+import { Alert } from "@mui/material";
+import { getMeshDBAPIEndpoint } from "@/app/endpoint";
+import { toast } from "react-toastify";
 
+type NNAssignRequestValues = {
+  install_number: string;
+  password: string;
+};
 
 export function NNAssignForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isDirty, isValid },
+  } = useForm<NNAssignRequestValues>();
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [networkNumber, setNetworkNumber] = useState("");
+
+  async function submitNNAssignRequest(input: NNAssignRequestValues) {
+    setIsSubmitted(true);
+    return fetch(`${await getMeshDBAPIEndpoint()}/api/v1/nn-assign/`, {
+      method: "POST",
+      body: JSON.stringify(input),
+    })
+      .then(async (response) => {
+        if (response.ok) {
+          const responseJson = await response.json();
+          setNetworkNumber(responseJson.network_number);
+        }
+      })
+      .catch(async (error) => {
+        const errorJson = await error.json();
+        const detail = await errorJson.detail;
+        toast.error(`Could not assign NN: ${detail}`);
+        setIsSubmitted(false);
+      });
+  }
+
+  const onSubmit: SubmitHandler<NNAssignRequestValues> = (data) => {
+    console.log(data);
+    submitNNAssignRequest(data);
+  };
+
   return (
     <>
       <div className={styles.formBody}>
-        <form action={sendForm}>
+        <form action={handleSubmit(onSubmit)}>
           <h2>Request Network Number</h2>
           <p>
             Enter an install number and the Pre-Shared Key, and receive a
@@ -20,14 +60,18 @@ export function NNAssignForm() {
           <br />
           <div className={styles.horizontal}>
             <input
+              {...register("install_number", {
+                required: "Please enter your first name",
+              })}
               type="number"
-              name="install_number"
               placeholder="Install Number"
               required
             />
             <input
+              {...register("password", {
+                required: "Please enter your password",
+              })}
               type="password"
-              name="password"
               placeholder="Pre-Shared Key"
               required
             />
@@ -35,8 +79,7 @@ export function NNAssignForm() {
           <div className={styles.centered}>
             <Button
               type="submit"
-              disabled={disableSubmitButton}
-              hidden={disableSubmitButton}
+              disabled={isSubmitted || !isDirty || !isValid}
               variant="contained"
               size="large"
               sx={{ width: "12rem", fontSize: "1rem", m: "1rem" }}
@@ -44,15 +87,15 @@ export function NNAssignForm() {
               Submit
             </Button>
           </div>
-          <h3 hidden={!disableSubmitButton} className={styles.nnLabel}>
-            Your Network Number:
-          </h3>
-          <h1 hidden={!disableSubmitButton} id="">
-            {networkNumber}
-          </h1>
         </form>
+
       </div>
-      <ToastContainer />
+        <div hidden={isNaN(parseInt(networkNumber))}>
+          <Alert>
+            <h3 className={styles.nnLabel}>Your Network Number:</h3>
+            <h1 id="">{networkNumber}</h1>
+          </Alert>
+        </div>
     </>
   );
 }
