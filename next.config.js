65f55e414fc96adae0402ae95de66d0b99a7fb5f
James Otten <jamesotten1@gmail.com>
2024-11-17 15:01:14 -0500

    Add security headers

--- next.config.js
+++ next.config.js
@@ -6,6 +6,35 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig = {
   output: "standalone",
   trailingSlash: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'accelerometer=(), ambient-light-sensor=(), autoplay=(), camera=(), display-capture=(), document-domain=(), encrypted-media=(), fullscreen=(), geolocation=(), gyroscope=(), interest-cohort=(), magnetometer=(), microphone=(), midi=(), payment=(), usb=()',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=30',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 module.exports = withNextIntl(nextConfig);
