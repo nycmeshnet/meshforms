e5b275de1e451803b00f5cbdb43dab4818170d97
Andrew Dickinson <andrewjd@amazon.com>
2024-11-19 18:17:38 -0500

    Fix test?

--- tests/00_join_form_meshdb_500.spec.ts
+++ tests/00_join_form_meshdb_500.spec.ts
@@ -66,5 +66,7 @@ test("meshdb is 500ing but succeed anyway", async ({ page }) => {
   // Then go home
   await page.waitForTimeout(1000);
   await page.locator("[name='home']").click();
-  await page.waitForURL("https://nycmesh.net/");
+  await page.waitForTimeout(1000);
+  const currentURL = new URL(page.url());
+  expect(currentURL.pathname).toBe("/");
 });
