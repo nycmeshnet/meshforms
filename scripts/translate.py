8b6a747f4c2e8092a5e23e28cad9cecc9ebb3ee3
Willard Nilges <willardnilges@gmail.com>
2024-11-29 00:48:39 -0500

    docmentation and don't translate something if it doesn't need it.

--- scripts/translate.py
+++ scripts/translate.py
@@ -60,43 +60,66 @@ def main():
 
     args = parser.parse_args()
 
-    # TODO: Argparse and logging
-    # Parse the en.json (TODO optionally pass another locale to use as source
-    # of truth)
-
-    # TODO: Print translations instead of wirting to file
+    # Parent locale is based on args. Set up some variables for it.
     parent_locale_name = args.parent_locale
     parent_locale_file = f"{parent_locale_name}.json"
-    locale_files = listdir("./messages")
-    locale_files.remove(parent_locale_file)
 
+    # Load in the parent locale
     parent_locale = json.load(open(f"./messages/{parent_locale_file}"))
-    # We're just interested in comparing what keys are missing from the translation file
+
+    # We're just interested in comparing what keys are missing from the
+    # translation file. More on that later.
     parent_locale_stripped = strip_values(parent_locale)
 
+    # Optionally set up Git
     if args.git_commits:
         repo = Repo()
     
+    # If there's a target locale specified, then limit the list to that one.
     if args.target_locale:
         locale_files = [f"{args.target_locale}.json"]
+    else:
+        # Get the rest of the locales and remove the parent locale from the list.
+        locale_files = listdir("./messages")
+        locale_files.remove(parent_locale_file)
+
     logging.info(f"Locales to translate: {locale_files}")
     for locale_file in locale_files:
+        # Set up variables for the current target locale
         locale_name = pathlib.Path(locale_file).stem
         locale_path = f"./messages/{locale_file}"
 
         logging.info(f"{parent_locale_name} -> {locale_name}")
+
+        # Load current locale in
         locale = json.load(open(locale_path))
 
+        # We strip the values out of the locales and focus on what keys are missing
+        # from the target locale and patching them in. Also remove keys that the parent locale lacks.
         # FIXME (wdn): There's a bug in here where it won't preserve values that have
         # moved.
+
+        # Strip values from the target locale
         locale_stripped = strip_values(locale)
+
+        # Diff the keys
         d = diff(locale_stripped, parent_locale_stripped)
 
+        if d == {}:
+            logging.warning("Diff is empty, skipping this locale and moving on.")
+            continue
+
+        # Patch them in
         patched_locale = jsondiff.patch(locale, d)
 
+
+        # Now that we know what keys are blank, we need to take that patched
+        # locale and the parent locale and call Google Translate to fill in the
+        # gaps.
         at = AutoTranslator(parent_locale_name, locale_name)
         translated_locale = at.auto_translate_parent_locale_into_patched_locale(patched_locale, parent_locale)
 
+        # Write and commit the translations
         if args.write or args.git_commits:
             logging.info("Writing translations to file...")
             with open(locale_path, 'w') as f:
