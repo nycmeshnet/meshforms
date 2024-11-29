5247861236ce9b032a3439e422aae4e08a23e9ea
Andrew Dickinson <andrewjd@amazon.com>
2024-11-28 22:48:40 -0500

    Formatting

--- src/components/JoinForm/JoinForm.tsx
+++ src/components/JoinForm/JoinForm.tsx
@@ -602,7 +602,9 @@ export default function JoinForm() {
           </div>
           <div className={styles.captchaDisclaimer}>
             This site is protected by reCAPTCHA and the Google
-            <a href="https://policies.google.com/privacy">Privacy Policy</a> and
+            <a href="https://policies.google.com/privacy">
+              Privacy Policy
+            </a> and
             <a href="https://policies.google.com/terms">
               Terms of Service
             </a>{" "}
