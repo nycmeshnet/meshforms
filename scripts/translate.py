5f4aa77e4f049aea4255e6e4a381776d635246a7
Willard Nilges <willardnilges@gmail.com>
2024-11-29 00:04:18 -0500

    Getting there, but there's some bugs with ´'s

--- scripts/translate.py
+++ scripts/translate.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from os import listdir
+import pathlib
 from typing import Any
 from jsondiff import diff
 import jsondiff
@@ -18,27 +19,33 @@ def strip_values(d: dict[str, Any]) -> dict[str, Any]:
 
     return r
 
-def auto_translate_parent_locale_into_patched_locale(patched_locale, parent_locale) -> dict[Any, Any]:
-    r = {}
-    translator = Translator()
-    for k, v in patched_locale.items():
-        if type(v) == dict:
-            r[k] = auto_translate_parent_locale_into_patched_locale(v, parent_locale[k])
-        elif v == "":
-            # TODO: pass locale
-            auto_translated_text = translator.translate(parent_locale[k], src="en", dest="es").text
-            logging.info(f"Translated missing text: '{parent_locale[k]}' -> '{auto_translated_text}'")
-            r[k] = auto_translated_text
-        else:
-            r[k] = v
+class AutoTranslator():
+    def __init__(self, src: str, dest: str) -> None:
+        self.src = src
+        self.dest = dest
 
-    return r
+    def auto_translate_parent_locale_into_patched_locale(self, patched_locale, parent_locale) -> dict[Any, Any]:
+        r = {}
+        translator = Translator()
+        for k, v in patched_locale.items():
+            if type(v) == dict:
+                r[k] = self.auto_translate_parent_locale_into_patched_locale(v, parent_locale[k])
+            elif v == "":
+                # TODO: pass locale
+                auto_translated_text = translator.translate(parent_locale[k], src=self.src, dest=self.dest).text
+                logging.info(f"Translated missing text: '{parent_locale[k]}' -> '{auto_translated_text}'")
+                r[k] = auto_translated_text
+            else:
+                r[k] = v
+
+        return r
 
 def main():
     # TODO: Argparse and logging
     # Parse the en.json (TODO optionally pass another locale to use as source
     # of truth)
     parent_locale_file = "en.json"
+    parent_locale_name = pathlib.Path(parent_locale_file).stem
     locale_files = listdir("./messages")
     locale_files.remove(parent_locale_file)
     parent_locale = json.load(open(f"./messages/{parent_locale_file}"))
@@ -48,6 +55,9 @@ def main():
 
     print(locale_files)
     for locale_file in locale_files:
+        locale_name = pathlib.Path(locale_file).stem
+
+        logging.info(f"{parent_locale_name} -> {locale_name}")
         locale = json.load(open(f"./messages/{locale_file}"))
 
         # FIXME (wdn): There's a bug in here where it won't preserve values that have
@@ -57,8 +67,13 @@ def main():
 
         patched_locale = jsondiff.patch(locale, d)
 
-        translated_locale = auto_translate_parent_locale_into_patched_locale(patched_locale, parent_locale)
-        print(json.dumps(translated_locale))
+        at = AutoTranslator(parent_locale_name, locale_name)
+        translated_locale = at.auto_translate_parent_locale_into_patched_locale(patched_locale, parent_locale)
+
+        logging.info("Writing translations to file...")
+        with open(f"./messages/{locale_file}", 'w') as f:
+            json.dump(translated_locale, f)
+
         break # TODO: Remove break
 
 
