829cf8886b212400790e691c166033486904a8e3
Will Nilges <willardnilges@gmail.com>
2025-05-12 22:50:06 -0400

    Delete landing page and redirect to join form

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,17 +1,11 @@
 import Landing from "@/components/Landing/Landing";
 import { setRequestLocale } from "next-intl/server";
+import { redirect } from "next/dist/server/api-utils";
 
 type Props = {
   params: { locale: string };
 };
 
-export default function Home({ params: { locale } }: Props) {
-  setRequestLocale(locale);
-  return (
-    <>
-      <main>
-        <Landing />
-      </main>
-    </>
-  );
+export default function Home() {
+  redirect("/en/join/");
 }
